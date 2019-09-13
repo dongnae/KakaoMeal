ce4d7d9fc90748a53e19f8e3b8b815880a170244
nnnlog <sorisem4106@naver.com>
2019-09-14 01:04:38 +0900

    add license

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/*  MealKakaoBot, 동래고등학교 급식 챗봇 프로그램
+    Copyright (C) 2019  박찬솔
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 process.env.TZ = "Asia/Seoul";
 
 const neis = require("neis");
